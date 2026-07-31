5eae1ba3f12328123bc841dc012b3eddd4c1f833
Aman Mishra <amanmishravk@gmail.com>
2026-07-31 20:47:33 +0530

    Time: 7 ms (59.04%), Space: 46.3 MB (36.45%) - LeetHub

---
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minFlips(String target) {
+        int n = target.length();
+        int flips = 0;
+        char status = '0';
+        for (int i = 0; i < n; i++) {
+            if (status != target.charAt(i)) {
+                flips++;
+            }
+            status = flips % 2 == 1 ? '1' : '0'; 
+        }
+        return flips;
+    }
+}
